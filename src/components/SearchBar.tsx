4ed1700d2cb84cc70f957852e127ba20521cb3b5
Marek Gromko <marek.gromko@hotmail.ca>
2025-09-14 13:14:35 -0400

    feat: end SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,38 @@
+import { useRef } from "react";
+
 interface SearchBarProps{
     placeholder?: string;
     onSearch?: (hint: string)=>any
 }
 
-const SearchBar = ()=>{
+const SearchBar = (props: SearchBarProps)=>{
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = () => {
+        if(props.onSearch && inputRef.current)
+            props.onSearch(inputRef.current.value);
+    }
+
+    const eraseInput = ()=>{
+        if(inputRef.current) {
+            inputRef.current.value = "";
+        }
+    }
+
     return (
         <div className="search-bar">
-            
+            <div className="input-bar">
+                <div className="t-icon i-search xlarge"/>
+                <form onSubmit={e=>(e.preventDefault(), handleSubmit())}>
+                    <input ref={inputRef} type="text" placeholder="Search..."/>
+                </form>
+                <button onClick={e=>(e.preventDefault(), eraseInput())} className="t-button pad-4">
+                    <div className="t-icon i-xmark xlarge"/>
+                </button>
+            </div>
+            <button onClick={handleSubmit} className="t-button pad-4">
+                <div className="t-icon i-arrow-refresh xlarge"/>
+            </button>
         </div>
     )
 }
