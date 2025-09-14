4acf08dce14eb65ee2f00145d7b8c53fd64b4b6e
Marek Gromko <marek.gromko@hotmail.ca>
2025-09-14 12:33:03 -0400

    add: comp PersonCell

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import PagedPanel from './components/PagedPanel'
+import PersonCell from './components/PersonCell'
+import { faker } from '@faker-js/faker';
+const makePerson = ()=>{
+  return {
+    id: faker.number.int(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    gender: faker.person.sex(),
+    email: faker.internet.email()
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <PagedPanel page={0} hasPrevPage={true} isWaiting={false}>
+        <PersonCell {...makePerson()}/>
+      </PagedPanel>
     </>
   )
 }
---
+++ src/components/PersonCell.tsx
@@ -0,0 +1,26 @@
+interface PersonCellProps{
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    email: string;
+
+}
+
+const PersonCell = (props: PersonCellProps) => {
+    return (
+        <div className="person-cell t-ibox small">
+            <div className="row id-gender">
+                <div className="id">
+                    <label>ID</label>
+                    <div>@{props.id}</div>
+                </div>
+                <div className="gender">{props.gender}</div>
+            </div>
+            <div className="row name">{props.firstName + " " + props.lastName}</div>
+            <div className="row email">{props.email}</div>
+        </div>
+    )
+}
+
+export default PersonCell;
