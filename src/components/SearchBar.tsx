79f72fab56d50baf3963ce5104f796bf60ba005b
Marek Gromko <marek.gromko@hotmail.ca>
2025-09-14 12:36:48 -0400

    init: Component SearchBar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+interface SearchBarProps{
+    placeholder?: string;
+    onSearch?: (hint: string)=>any
+}
+
+const SearchBar = ()=>{
+    return (
+        <div className="search-bar">
+            
+        </div>
+    )
+}
+
+export default SearchBar;
